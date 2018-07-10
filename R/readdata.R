92f5837bef5bb8e6dffd3d71c4eeba470ab53867
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2018-07-10 10:17:21 -0400

    Start incorporating T and P
    assume CMIP5 naming convention to start. Once comfortable in science,
    can be more diverse in input structures

--- R/readdata.R
+++ R/readdata.R
@@ -119,6 +119,127 @@ read.temperatures <- function(filename, len=NULL, tag=basename(filename), varnam
 }
 
 
+#' Read ESM precipitation data
+#'
+#' The precipitation data should be a netCDF file of average annual grid values.
+#' The data is assumed to have its dimensions of time, lat, lon, where lon is
+#' the most rapidly varying index, and time is the least.
+#'
+#' The output will be a list with six fields:
+#' \describe{
+#'   \item{\strong{pr}}{Matrix of precipitation data.}
+#'   \item{\strong{pgop}}{Vector operator for global mean precipitation.}
+#'   \item{\strong{lat}}{Vector of latitude values.  These are only replicated
+#' once, so their length is nlat, not ngrid.  You have to use \code{rep(lat,
+#' nlon)} to get latitudes for each grid cell.}
+#'   \item{\strong{lon}}{Vector of longitude values.  These are only replicated
+#' once, so their lingth is nlon, not ngrid.  Getting longitudes for each grid
+#' cell is tricksy.  Try \code{as.vector(matrix(rep(lon, nlat), nrow=nlat,
+#' byrow=TRUE))}.  Fortunately, you don't need them very often.}
+#'   \item{\strong{time}}{Vector of time values, given as years since the base
+#' year of the dataset.}
+#'   \item{\strong{tags}}{A list of datasets that were concatenated to get this
+#' structure.  The names of the list are the tags given to the dataset, and the
+#' values are a vector of start-row, end-row pairs.}
+#' }
+#'
+#' The data at each time is represented as a flattened vector of grid cells.
+#' The flattening is performed by transposing to lat, lon ordering, so that lat
+#' will be the most rapidly varying index (because R uses
+#' Fortran-style indexing).  Then the spatial dimensions are discarded,
+#' resulting in a 1D vector.  The time dimension is kept, resulting in a matrix
+#' with years in rows and grid cells in columns.  The dimensions of the matrix
+#' will be Nyear x Ngrid, where Nyear is the number of years in the data set,
+#' and Ngrid is the number of grid cells.
+#'
+#' The \code{pgop} vector is a vector whose dot product with a flattened grid is
+#' the area-weighted global mean for the grid.  It is stored as a column vector,
+#' so \code{pr \%*\% pgop} is the time series of global mean precipitations.
+#'
+#' The lat and lon dimension variables from the input file  are also
+#' stored in the structure.  These are primarily useful for writing out
+#' generated grids as netCDF files.  The time dimension variable is converted to
+#' integer years, starting at 0.
+#'
+#' Conventionally, we refer to the output list as \code{griddata}.  Notably, any
+#' other function with a \code{griddata} argument is expecting one of these
+#' structures.
+#'
+#' @param filename Name of the input netCDF file
+#' @param len Maximum length of the time series to read.  If the data read is
+#' longer, it will be trimmed.  (Default: read entire time series, regardless of
+#' length.)
+#' @param varname Name of the variable to read from the netcdf file.
+#' Irrespective of the name in the netcdf file, it will be called 'tas' in the
+#' return data.
+#' @param tag A string identifying the name of the scenario.  If omitted, the
+#' tag will default to the filename.
+#' @param latvar Name of the latitude dimension variable in the input file.
+#' @param lonvar Name of the longitude dimension variable in the input file.
+#' @param timevar Name of the time dimension variable in the input file.
+#' @return A \code{griddata} list (see details).
+#' @importFrom assertthat assert_that
+#' @export
+read.precipitations <- function(filename, len=NULL, tag=basename(filename), varname='pr',
+                              latvar='lat', lonvar='lon', timevar='time')
+{
+    pann <- ncdf4::nc_open(filename)
+
+    ## pr3d should have dimensions of time x lat x lon in the netcdf file.
+    ## Because R uses Fortran array layout, this gets reversed to lon x lat x time.
+    pr3d <- ncdf4::ncvar_get(pann, var=varname)
+    lat <- ncdf4::ncvar_get(pann, var=latvar)
+    nlat <- length(lat)
+    lon <- ncdf4::ncvar_get(pann, var=lonvar)
+    nlon <- length(lon)
+    time <- ncdf4::ncvar_get(pann, var=timevar)
+    ntime <- length(time)
+    timeout <- seq_along(time) - 1
+    ncdf4::nc_close(pann)
+
+    assert_that(all(dim(pr3d) == c(nlon, nlat, ntime)))
+
+    if(!is.null(len)) {
+        ## Trim the input to the requested length.
+        if(ntime < len) {
+            warning('Input time series shorter than requested length.  Requested: ', len, ' Read: ', ntime)
+        }
+        else {
+            ntime <- len
+            time <- time[1:ntime]
+            timeout <- timeout[1:ntime]
+            pr3d <- pr3d[ , , 1:ntime]
+        }
+    }
+
+
+    ## reorganize and flatten the 3-D array into a 2d array of ntime x ngrid
+    ## As we do this, we will also make latitude the most rapidly varying index
+    ## for the individual time slices.
+
+    pr <- aperm(pr3d, c(3,2,1))
+    dim(pr) <- c(ntime, nlat*nlon)
+
+    ## create a grid cell area factor that has the same shape as the flattened
+    ## grid.  Since latitude is the most rapidly varying dimension in the grid,
+    ## we just need to replicate cos(latitude), nlon times
+    areafac <- rep(cos(lat * pi/180.0), times=nlon)
+    ## normalize areafac so that sum(area*grid) is the global weighted average
+    areafac <- areafac/sum(areafac)
+
+    ## The tag element of the output is a named list of 2-element vectors,
+    ## giving the start and end of the data in the output.  When datasets are
+    ## concatenated, this allows us to recover the originals
+    tagout <- list(c(1,ntime))
+    names(tagout) <- tag
+
+    gd <- list(pr=as.matrix(pr), pgop=as.matrix(areafac), lat=lat, lon=lon,
+               time=timeout, tags=tagout)
+    class(gd) <- 'griddata'
+    gd
+}
+
+
 #' @rdname saving_and_restoring
 #' @export
 loadmodel <- function(file)
@@ -284,3 +405,5 @@ c.griddata <- function(...)
 {
     concatGrids(list(...))
 }
+
+
---
+++ R/trainTP.R
@@ -0,0 +1,121 @@
+#' Train the emulator on a set of input ESM temperature and precipitation data.
+#'
+#' This function runs all of the steps in the analysis that depend only on the
+#' input data.  The results are saved in a structure of class \code{fldgen}.
+#' This structure contains everything the emulator has learned about the model.
+#' Therefore, it can be saved and reloaded in future sessions to bypass the
+#' analysis step.
+#'
+#' The steps run by this procedure are \code{\link{read.temperatures}},
+#' \code{\link{read.precipitations}},
+#' \code{\link{pscl_analyze}}, and \code{\link{eof_analyze}}.  Additionally,
+#' the global mean temperature, global mean precipitation, and the FFT of the
+#' EOF decomposition are calculated and stored.  For backward compatibility,
+#' all of those functions remain part of the public interface.
+#'
+#' At present the field generation functions still take as arguments the
+#' individual components of the structure; however, in the next version they
+#' will expect a \code{fldgen} object, which can be constructed manually using
+#' \code{\link{fldgen_object}}.
+#'
+#' The names of the input files are recorded in the \code{infiles} field of the
+#' returned object.  This is for the your information only; none of the code
+#' here does anything with the recorded filenames.  You have the choice of
+#' recording either absolute filenames (useful for determining exactly which
+#' dataset was used, if you have a lot of similarly named files) or relative
+#' filenames (useful if you might distribute a saved model to other users who
+#' might not have the same directory structure as you).
+#'
+#' @param dat A single directory name, or a list of netCDF files.  If a
+#' directory name is given, all netCDF files in the directory will be used.
+#' @param latvar Name of the latitude coordinate variable in the netCDF files.
+#' Normally this is \code{'lat'}, but occasionally it will be something
+#' different, such as \code{'lat_2'}.
+#' @param lonvar Name of the longitude coordinate variable in the netCDF files.
+#' @param meanfield Function to compute the mean temperature response field.
+#' The default is a linear pattern scaling algorithm.
+#' @param record_absolute If \code{TRUE}, record absolute paths for the input
+#' files; otherwise, record relative paths.
+#' @return A \code{fldgen} object.
+#' @export
+trainTP <- function(dat, latvar='lat', lonvar='lon', meanfield=pscl_analyze,
+                  record_absolute=FALSE)
+{
+    if(length(dat) == 1 && file.info(dat)$isdir) {
+        ## This is a directory.  Replace with the list of netCDF files contained
+        ## within.
+        dat <- list.files(dat, '\\.nc$', full.names=TRUE)
+    }
+
+    if(record_absolute) {
+        infiles <- normalizePath(dat)
+    }
+    else {
+        infiles <- dat
+    }
+
+
+    return(dat)
+
+
+    readin <- function(fn) {read.temperatures(fn, latvar=latvar, lonvar=lonvar)}
+
+
+
+
+    griddata <- concatGrids(lapply(dat, readin)) # information about the
+                                                 # temperature grid
+    tgav <- griddata$tas %*% griddata$tgop # Global mean temperature
+
+    meanfld <- meanfield(griddata$tas, tgav)
+
+    reof <- eof_analyze(meanfld$r, griddata$tgop)
+
+    reof_l <- split_eof(reof, griddata)
+    psd <- psdest(reof_l)
+
+    fldgen_object(griddata, tgav, pscl, reof, sqrt(psd$psd), psd$phases, infiles)
+}
+
+#' Create a \code{fldgen} object from constituent parts
+#'
+#' Normally this code will be called internally, but it is available for
+#' external use, so that data created using the old interface can be converted.
+#'
+#' If there is only a single ESM run in the input data set, then \code{fxphase}
+#' can be passed as a matrix of phases (instead of a list of matrices).  It will
+#' be converted into a list automatically.
+#'
+#' @param griddata An object returned from \code{\link{read.temperatures}} or
+#' \code{\link{concatGrids}}.
+#' @param tgav Global mean temperature for the grids in griddata.
+#' @param pscl Object returned from \code{\link{pscl_analyze}}.
+#' @param reof Object returned from \code{\link{eof_analyze}}.
+#' @param fxmag The magnitude of the Fourier transform of the EOF projection
+#' coefficients.  This should be a matrix [Ntime x NEOF].  If using
+#' \code{\link{psdest}}, note this is the square root of the power spectral
+#' density returned by that function.
+#' @param fxphase List of matrices [Ntime x NEOF] of phases of the Fourier
+#' transform.  There should be one element in the list for each input ESM run.
+#' @param infiles Names of input files used to construct the data.
+#' @export
+#' @keywords internal
+fldgen_object <- function(griddata, tgav, pscl, reof, fxmag, fxphase, infiles)
+{
+    if(is.matrix(fxphase)) {
+        phases <- list(fxphase)
+    }
+    else {
+        phases <- fxphase
+    }
+    Fx <- list(mag = fxmag,
+               phases = phases)
+    fg <- list(griddata=griddata,
+               tgav=tgav,
+               pscl=pscl,
+               reof=reof,
+               fx=Fx,
+               infiles=infiles)
+    class(fg) <- 'fldgen'
+    fg
+}
