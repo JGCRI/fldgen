c447ac350dd1804d1bbb7380b7194bf13a7964ed
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2018-07-27 15:46:42 -0400

    generalized to work on matrices

--- R/normalizeresiduals.R
+++ R/normalizeresiduals.R
@@ -45,8 +45,11 @@ normalize.resids <- function(r, len=NULL){
         }
     }
 
-    # Get the quantiles of every residual for the grid cell
-    ecdf(r)(r) -> quantiles
+    # Get the quantiles of every residual for each grid cell
+    # ecdf(r)(r) -> quantiles
+    # quanfun <- apply(r, 2, ecdf)
+    get.quans <- function(x){stats::ecdf(x)(x)}
+    quantiles <- apply(r,2, get.quans)
 
 
     # The quantile corresponding to the largest residual will be 1. That will
@@ -63,18 +66,196 @@ normalize.resids <- function(r, len=NULL){
         }
         sort(x,partial=len-N+1)[len-N+1]
     }
-    maxN(quantiles) -> max2
+
+    max2 <- maxN(quantiles[,1]) # apparently it's the same for all grid cells even though the
+                                # quantiles vary by grid cells.
 
     # use the second largest to create our offset from 1 for the largest quantile
-    (1-max2) / 5 -> offset
-    quantiles[which.max(quantiles)] <- max(quantiles) - offset
+    offset <- (1-max2) / 5
+    offset.max <- function(x){
+        x[which.max(x)] <- max(x) - offset
+        return(x)}
+
+    q2 <- apply(quantiles, 2, offset.max)
 
 
     # get the values of the normal distribution corresponding to these quantiles.
-    normresids <- qnorm(quantiles, mean = 0, sd = 1)
+    normresids <- apply(q2, 2, stats::qnorm)
 
 
     # return
-    list(r = r, quants = quantiles, rn = normresids)
+    output <- list(r = r, quants = q2, rn = normresids)
+    class(output) <- 'quantilemapping'
+
+    return(output)
 
 }
+
+
+#' unNormalize residuals
+#'
+#' A vector of residuals must be normalized for the fldgen algorithm to work
+#' properly. This function takes a vector of residuals (T or P), calculates the
+#' quantiles of each residual, and maps the residual to the corresponding
+#' quantile value in a normal distribution.
+#'
+#' The output will be a list with Three fields:
+#' \describe{
+#'   \item{\strong{r}}{Column vector of the original, input residuals for a grid cell}
+#'   \item{\strong{quants}}{Column vector of the quantiles of each input residual.}
+#'   \item{\strong{rn}}{Column vectorof the new, normally distributed residuals.}
+#' }
+#'
+#' UPDATE: The data at each time is represented as a flattened vector of grid cells.
+#' The flattening is performed by transposing to lat, lon ordering, so that lat
+#' will be the most rapidly varying index (because R uses
+#' Fortran-style indexing).  Then the spatial dimensions are discarded,
+#' resulting in a 1D vector.  The time dimension is kept, resulting in a matrix
+#' with years in rows and grid cells in columns.  The dimensions of the matrix
+#' will be Nyear x Ngrid, where Nyear is the number of years in the data set,
+#' and Ngrid is the number of grid cells.
+#'
+#'
+#' Conventionally, we refer to the output list as \code{griddata}.  Notably, any
+#' other function with a \code{griddata} argument is expecting one of these
+#' structures.
+#'
+#' @param r Column vector of the original, input residuals for a grid cell
+#' @param len Maximum length of the time series to read.  If the data read is
+#' longer, it will be trimmed.  (Default: read entire time series, regardless of
+#' length.)
+#' @export
+normalize.resids <- function(r, len=NULL){
+
+    ntime <- length(r)
+    if(!is.null(len)) {
+        ## Trim the input to the requested length.
+        if(ntime < len) {
+            warning('Input time series shorter than requested length.  Requested: ', len, ' Read: ', ntime)
+        }
+        else {
+            ntime <- len
+            r <- r[1:ntime]
+        }
+    }
+
+    # Get the quantiles of every residual for each grid cell
+    # ecdf(r)(r) -> quantiles
+    # quanfun <- apply(r, 2, ecdf)
+    get.quans <- function(x){stats::ecdf(x)(x)}
+    quantiles <- apply(r,2, get.quans)
+
+
+    q2 <- quantiles # apply(quantiles, 2, offset.max)
+
+
+    # get the values of the normal distribution corresponding to these quantiles.
+    normresids <- apply(q2, 2, stats::qnorm)
+    normresids[is.infinite(normresids)] <- 10
+
+
+    # return
+    output <- list(r = r, quants = q2, rn = normresids)
+    class(output) <- 'quantilemapping'
+
+    return(output)
+
+}
+
+
+
+#' Normalize residuals
+#'
+#' A vector of residuals must be normalized for the fldgen algorithm to work
+#' properly. This function takes a vector of residuals (T or P), calculates the
+#' quantiles of each residual, and maps the residual to the corresponding
+#' quantile value in a normal distribution.
+#'
+#' The output will be a list with Three fields:
+#' \describe{
+#'   \item{\strong{r}}{Column vector of the original, input residuals for a grid cell}
+#'   \item{\strong{quants}}{Column vector of the quantiles of each input residual.}
+#'   \item{\strong{rn}}{Column vectorof the new, normally distributed residuals.}
+#' }
+#'
+#' UPDATE: The data at each time is represented as a flattened vector of grid cells.
+#' The flattening is performed by transposing to lat, lon ordering, so that lat
+#' will be the most rapidly varying index (because R uses
+#' Fortran-style indexing).  Then the spatial dimensions are discarded,
+#' resulting in a 1D vector.  The time dimension is kept, resulting in a matrix
+#' with years in rows and grid cells in columns.  The dimensions of the matrix
+#' will be Nyear x Ngrid, where Nyear is the number of years in the data set,
+#' and Ngrid is the number of grid cells.
+#'
+#'
+#' Conventionally, we refer to the output list as \code{griddata}.  Notably, any
+#' other function with a \code{griddata} argument is expecting one of these
+#' structures.
+#'
+#' @param r Column vector of the original, input residuals for a grid cell
+#' @param len Maximum length of the time series to read.  If the data read is
+#' longer, it will be trimmed.  (Default: read entire time series, regardless of
+#' length.)
+#' @export
+normalize.resids <- function(r, len=NULL){
+
+    ntime <- length(r)
+    if(!is.null(len)) {
+        ## Trim the input to the requested length.
+        if(ntime < len) {
+            warning('Input time series shorter than requested length.  Requested: ', len, ' Read: ', ntime)
+        }
+        else {
+            ntime <- len
+            r <- r[1:ntime]
+        }
+    }
+
+    # Get the quantiles of every residual for each grid cell
+    # ecdf(r)(r) -> quantiles
+    # quanfun <- apply(r, 2, ecdf)
+    get.quans <- function(x){stats::ecdf(x)(x)}
+    quantiles <- apply(r,2, get.quans)
+
+
+    # The quantile corresponding to the largest residual will be 1. That will
+    # map to a normalized residual of Inf, which will break the field
+    # generation. Ensure that it remains the largest quantile, but replace
+    # with a value < 1.
+
+    # Get the second largest quantile
+    maxN <- function(x, N=2){
+        len <- length(x)
+        if(N>len){
+            warning('N greater than length(x).  Setting N=length(x)')
+            N <- length(x)
+        }
+        sort(x,partial=len-N+1)[len-N+1]
+    }
+
+    max2 <- maxN(quantiles[,1]) # apparently it's the same for all grid cells even though the
+    # quantiles vary by grid cells.
+
+    # use the second largest to create our offset from 1 for the largest quantile
+    offset <- (1-max2) / 5
+    offset.max <- function(x){
+        x[which.max(x)] <- max(x) - offset
+        return(x)}
+
+    q2 <- apply(quantiles, 2, offset.max)
+
+
+    # get the values of the normal distribution corresponding to these quantiles.
+    normresids <- apply(q2, 2, stats::qnorm)
+
+
+    # return
+    output <- list(r = r, quants = q2, rn = normresids)
+    class(output) <- 'quantilemapping'
+
+    return(output)
+
+}
+
+
+
