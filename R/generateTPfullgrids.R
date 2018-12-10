f9b400db7241835ca0b9c3249f06c4b62f4ea655
Robert Link <robert.link@pnnl.gov>
2018-12-10 09:10:15 -0500

    Simplify generate.TP.fullgrids()
    Several changes and refactorings to streamline this function.  Most
    notably, some of the elements of the output were removed.  The inverse
    transform functions were inputs to this function; thus, there was no
    need to copy them to the output.  The coordinate array is already
    stored in the griddata object, so there is also no need to recreate it
    here.

--- R/generateTPfullgrids.R
+++ R/generateTPfullgrids.R
@@ -43,78 +43,46 @@
 #' if it looks like there may be an issue.
 #' @param reconstruction_function A function for constructing a mean field from
 #' trained pattern scaling result + a vector of global annual mean temperatures.
-#' @param addNAs A logical TRUE/FALSE, whether NA values need to be added back
-#' to the full grids of data (that is, if ISIMIP data, restricting to land cells
-#' only, was used for training rather than data with full global coverage.)
-#' Defaults to FALSE.
 #' @return A list of:
 #' 1) fullgrids = A list of new full fields, each entry in
 #' the list is a new realization, a matrix that is [Nyears x 2 * Ngrid]; the
 #' first 1:Ngrid cols are the temperature field and columns
 #' (Ngrid + 1):(2*Ngrid) are the precipitation field.
-#' 2) coordinates = something KD needs to fill in, relevant to using ISIMIP data
-#' in the grand experiment.
-#' 3) time = the input time column used to subset the trained emulator so that
+#' 2) time = the input time column used to subset the trained emulator so that
 #' pattern scaling information from the appropriate years is applied to tgav
 #' time series. Note: grids cannot be generated for tgav years missing from
 #' the trained emulator.
-#' 4) meanfieldT = the reconstructed, pattern scaled temperature mean field.
-#' 5) meanfieldP = the reconstructed, pattern scaled precipitation mean field.
-#' 6) tvarunconvert_fcn = the tvarunconvert_fcn input.
-#' 7) pvarunconvert_fcn = the pvarunconvert_fcn input.
+#' 3) meanfieldT = the reconstructed, pattern scaled temperature mean field.
+#' 4) meanfieldP = the reconstructed, pattern scaled precipitation mean field.
 #' @export
 
 generate.TP.fullgrids <- function(emulator, residgrids, tgavdf,
                                   tvarunconvert_fcn = NULL, pvarunconvert_fcn = exp,
-                                  reconstruction_function = pscl_apply, addNAs = FALSE){
-
-    # silence package checks
-    column_index <- lat <- lon <- NULL
+                                  reconstruction_function = pscl_apply){
 
     # dbl check that convert and unconvert functions are actual inverses on some test data.
     # throw a warning if this is not the case but it is up to the user to track down.
-    testx <- 1:5
 
-    if(!is.null(emulator$griddataT$tvarconvert_fcn) &
-       !(is.null(tvarunconvert_fcn))){
-        testx %>%
-            emulator$griddataT$tvarconvert_fcn() %>%
-            tvarunconvert_fcn() ->
-            x1
+    if(!is.null(emulator$griddataT$tvarconvert_fcn)) {
+        if(is.null(tvarunconvert_fcn)) {
+            stop('Temperature transform function was applied, but no inverse function supplied.')
+        }
 
-        testx %>%
-            tvarunconvert_fcn() %>%
-            emulator$griddataT$tvarconvert_fcn() ->
-            x2
-
-        if((max(abs(x1-testx))>1e-14) | (max(abs(x2-testx))>1e-14) ){
+        if(!chkinverse(emulator$griddataT$tvarconvert_fcn, tvarunconvert_fcn,
+                       range=c(250,310))) {
             warning('Your functions for transforming and inverse transforming the support of T may not be inverses of each other 1.')
         }
-
-    } else if((!is.null(emulator$griddataT$tvarconvert_fcn) & is.null(tvarunconvert_fcn)) |
-              (is.null(emulator$griddataT$tvarconvert_fcn) & !is.null(tvarunconvert_fcn))){
-        warning('Your functions for transforming and inverse transforming the support of T may not be inverses of each other 2.')
     }
 
-    if(!is.null(emulator$griddataP$pvarconvert_fcn) &
-       !(is.null(pvarunconvert_fcn))){
-        testx %>%
-            emulator$griddataP$pvarconvert_fcn() %>%
-            pvarunconvert_fcn() ->
-            x1
-
-        testx %>%
-            pvarunconvert_fcn() %>%
-            emulator$griddataP$pvarconvert_fcn() ->
-            x2
-
-        if((max(abs(x1-testx))>1e-14) | (max(abs(x2-testx))>1e-14) ){
-            warning('Your functions for transforming and inverse transforming the support of P may not be inverses of each other 1.')
+    if(!is.null(emulator$griddataP$pvarconvert_fcn)) {
+        if(is.null(pvarunconvert_fcn)) {
+            stop('Precipitation transform function was applied, but no inverse function supplied.')
         }
 
-    } else if((!is.null(emulator$griddataP$pvarconvert_fcn) & is.null(pvarunconvert_fcn)) |
-              (is.null(emulator$griddataP$pvarconvert_fcn) & !is.null(pvarunconvert_fcn))){
-        warning('Your functions for transforming and inverse transforming the support of P may not be inverses of each other 2.')
+        if(!chkinverse(emulator$griddataP$pvarconvert_fcn, pvarunconvert_fcn,
+                       range=c(1e-9, 1e-5), logspace=TRUE)) {
+            warning('Your functions for transforming and inverse transforming the support of P may not be inverses of each other 1.')
+        }
     }
 
 
@@ -204,54 +172,36 @@ generate.TP.fullgrids <- function(emulator, residgrids, tgavdf,
         fullgrids
 
 
+    return(list(fullgrids = fullgrids, time = time,
+                meanfieldT = meanfieldT, meanfieldP = meanfieldP))
+
+}
 
 
-    # Create the croodinates mapping file.
-    # TODO there is probably a better way to do this
-    # Create a coordinate mapping data frame. Since
-    # the lat is the most rapidly varying index left
-    # join the lat to a lon tibble.
-    tibble::tibble(lat = emulator$griddataT$lat) %>%
-        dplyr::mutate(join = 1) ->
-        lat_tibble
+#' Check if two functions are inverses of one another over a given range of inputs.
+#'
+#' This is a quick and dirty check; it will definitely fail if the input range
+#' is not in the domain of both functions.
+#'
+#' @param f1 First function to test
+#' @param f2 Second function to test
+#' @param range Range of test values
+#' @param ntest Number of values to test in the range
+#' @param logspace Flag indicating whether to space test values logarithmically.
+#' @return logical
+#' @keywords internal
+chkinverse <- function(f1, f2, range=c(1.0, 5.0), ntest=10, logspace=FALSE)
+{
+    if(logspace)
+        range <- log(range)
 
-    tibble::tibble(lon = emulator$griddataT$lon) %>%
-        dplyr::mutate(join = 1) %>%
-        dplyr::left_join(lat_tibble, by = "join") %>%
-        dplyr::mutate(column_index = 1:ncol(emulator$griddataT$vardata)) %>%
-        dplyr::select(column_index, lat, lon) ->
-        coordinates
+    xvals <- seq(range[1], range[2], length.out = ntest)
 
-    # If griddata mapping files exist then then add NA grid cells will have to be added back into
-    # the grids in order to return output that has the same grid dimensions.
-    if(!is.null(emulator$griddataT$mapping) & addNAs){
+    if(logspace)
+        xvals <- exp(xvals)
 
-        # Since the T and P fields are concatenated together in a single data frame the mapping data must be updated to
-        # reflect the single data frame.
-        emulator$griddataT$mapping %>%
-            dplyr::bind_rows(tibble::tibble(original_index = emulator$griddataP$mapping$original_index + max(emulator$griddataT$mapping$original_index),
-                                            new_index = emulator$griddataP$mapping$new_index + max(emulator$griddataT$mapping$new_index, na.rm = TRUE))) ->
-            mapping
+    xt1 <- f1(f2(xvals))
+    xt2 <- f2(f1(xvals))
 
-        # Use the new mapping file to insert the NAs into the fullgrids
-        fullgrids <- lapply(fullgrids, FUN = add_NAs, mapping = mapping)
-
-    } else if(!is.null(emulator$griddataT$mapping) & !addNAs){
-
-        # If the NAs were removed but the user has selected NOT to add the NAs back
-        # to the grid then discard the NA cells from the coordinates mapping file.
-        emulator$griddataT$mapping %>%
-            na.omit %>%
-            dplyr::select(column_index = new_index, lat, lon) ->
-            coordinates
-
-    }
-
-
-
-
-    return(list(fullgrids = fullgrids, coordinates = coordinates, time = time,
-                meanfieldT = meanfieldT, meanfieldP = meanfieldP,
-                tvarunconvert_fcn = tvarunconvert_fcn, pvarunconvert_fcn = pvarunconvert_fcn))
-
+    isTRUE(all.equal(xt1, xvals)) && isTRUE(all.equal(xt2, xvals))
 }
