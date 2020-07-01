5471bfbf24d272e275f759afd8e059d976394dd9
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2020-07-01 13:00:40 -0400

    add files from /pic/projects/GCAM/GE/drought-expt/fldgen-emulators

---
+++ inst/scripts/train-emulators.R
@@ -0,0 +1,25 @@
+library('fldgen')
+
+train_models <- function(models, tasvar='tasAdjust', prvar='prAdjust',
+                         datadir='./training-data') {
+
+    ## The following would give you the complete set of models:
+    ## models <- c('GFDL-ESM2M', 'HadGEM2-ES', 'IPSL-CM5A-LR', 'MIROC5')
+
+    for (model in models) {
+        datafiles <- list.files(path=datadir, pattern=model, full.names=TRUE)
+        cat('Processing model ', model, '  datafiles:\n', paste(datafiles, collapse='\n'),'\n')
+        emu <- trainTP(datafiles, tvarname=tasvar, pvarname=prvar)
+        emu$griddataP$vardata_raw <- NULL
+        outfilename <- paste0('fldgen-',model, '.rds')
+        coord <- emu$griddataT$coord
+        coord[67382, ] <- c(-49.75, 178.75)
+        emu$griddataT$coord <- coord
+        emu$griddataP$coord <- coord
+
+
+        saveRDS(emu, outfilename)
+    }
+}
+
+
