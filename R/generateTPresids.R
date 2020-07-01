4daa68d0db3966732f1a14a9e625a864acac0b25
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2020-07-01 14:32:35 -0400

    actually not vector, it's an NgridX1 matrix :-|

--- R/generateTPresids.R
+++ R/generateTPresids.R
@@ -23,7 +23,7 @@
 #' @export
 generate.TP.resids <- function(emulator, ngen, method = 1){
 
-    Ngrid <- length(emulator$meanfldT$w)
+    Ngrid <- nrow(emulator$meanfldT$w)
 
     newgrids <- lapply(1:ngen,
                        function(x) {
